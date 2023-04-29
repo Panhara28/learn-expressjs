71de4b3c07530add94c1908d8b205322e3ad1f3d
aipanhara <mcpanhara@gmail.com>
2023-04-29 14:17:38 +0700

    learn about next function

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,21 @@ const StartApplication = () => {
   router.use(express.urlencoded({ extended: true }));
   router.use(express.json());
 
+  // Middleware function that logs the incomming request URL
   router.use((req: Request, res: Response, next: NextFunction) => {
+    Logging.info(`Incoming request URL: ${req.url}`);
     next();
+  });
+
+  // Middleware function that sets a custom response header
+  router.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader("X-Custom-Header", "Hello, world!");
+    next();
+  });
+
+  // Route handler for GET /example
+  router.get("/example", (req: Request, res: Response) => {
+    res.send("This is and example route");
   });
 
   router.listen(port, () => Logging.info(`Server is running on port ${port}`));
