b28a9545c4ef69bb57c085b25d14f995030a6bc3
aipanhara <mcpanhara@gmail.com>
2023-04-29 14:02:15 +0700

    add express.json function

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import Logging from "./libs/Logging";
 
 const port = process.env.PORT ? process.env.PORT : 8080;
 const router = express();
 
-router.use(express.urlencoded({ extended: true }));
-
 const StartApplication = () => {
+  router.use(express.urlencoded({ extended: true }));
+  router.use(express.json());
+
+  router.use((req: Request, res: Response, next: NextFunction) => {
+    next();
+  });
+
   router.listen(port, () => Logging.info(`Server is running on port ${port}`));
 };
 
