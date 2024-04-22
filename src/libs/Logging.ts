5a0ccaaef7122dc0e488c2ee8c1c1197dad02337
panhara28 <titpanhara@gmail.com>
2024-04-22 14:52:35 +0700

    [Update]: type any to string

--- src/libs/Logging.ts
+++ src/libs/Logging.ts
@@ -1,38 +1,38 @@
 import chalk from "chalk";
 export default class Logging {
-  public static log = (args: any) => this.info(args);
+  public static log = (args: string) => this.info(args);
 
-  public static info = (args: any) =>
+  public static info = (args: string) =>
     console.log(
       chalk.blue(`[${new Date().toLocaleString()}] [INFO]`),
       typeof args === "string" ? chalk.blueBright(args) : args
     );
 
-  public static warn = (args: any) =>
+  public static warn = (args: string) =>
     console.log(
       chalk.yellow(`[${new Date().toLocaleString()}] [INFO]`),
       typeof args === "string" ? chalk.yellowBright(args) : args
     );
 
-  public static error = (args: any) =>
+  public static error = (args: string) =>
     console.log(
       chalk.red(`[${new Date().toLocaleString()}] [INFO]`),
       typeof args === "string" ? chalk.red(args) : args
     );
 
-  public static route = (args: any) => 
+  public static route = (args: string) => 
       console.log(
         chalk.cyan(`[${new Date().toLocaleString()}] [ROUTE]`,
         typeof args === "string" ? chalk.cyanBright(args) : args)
       )
 
-  public static not_found = (args: any) => 
+  public static not_found = (args: string) => 
       console.log(
         chalk.red(`[${new Date().toLocaleString()}] [Unknow Route]`,
         typeof args === "string" ? chalk.red(args) : args)
       )
 
-  public static mysql_log = (args: any) => 
+  public static mysql_log = (args: string) => 
       console.log(
         chalk.blue(`[${new Date().toLocaleString()}] [MySQL]`,
         typeof args === "string" ? chalk.blue(args) : args)
